0c8a82c8b23de405c9c00c5f9f1633df23ba02c6
Proser-V <10530@holbertonstudents.com00>
2025-08-13 12:14:33 +0200

    Numbers0to99.java

---
+++ 0x01/numbers/Numbers0to99.java
@@ -0,0 +1,9 @@
+public class Numbers0to99 {
+    public static void main(String[] args) {
+        System.err.printf("0");
+        for (int i = 1; i < 100; i++) {
+            System.out.printf(", %d", i);
+        }
+        System.err.printf("%n");
+    }
+}
