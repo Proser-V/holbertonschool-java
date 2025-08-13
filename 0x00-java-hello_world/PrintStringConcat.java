2b056d872b80df7c64555ab91143e5cf0830b2ed
Proser-V <10530@holbertonstudents.com00>
2025-08-13 11:49:19 +0200

    PrintStringConcat.java

---
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -0,0 +1,8 @@
+public class PrintStringConcat {
+    public static void main(String[] args) {
+        String text1 = "Holberton";
+        String text2 = "School";
+        String text3 = text1 + " " + text2;
+        System.out.println("Welcome to " + text3 + " !");
+    }
+}
